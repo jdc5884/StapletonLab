6de7f0e453ca342919f33eb44525248c4b8bf0e1
Julia Castillo <jdc5884@uncw.edu>
2019-04-19 12:34:53 -0400

    Uploading weekly Summary

--- StressSplicing/qPCR/comb_ratio_example.R
+++ StressSplicing/qPCR/comb_ratio_example.R
@@ -4,6 +4,7 @@ a = c(1,2,3,4,5,6)
 b = c(7,8,9,10,11,12)
 m = data.frame(cbind(name,a,b), stringsAsFactors = FALSE)
 
+# this function divides every item in column 1 by every item in column 2
 divide <- function(col1, col2){
   r = c()
   for (i in col1){
@@ -12,21 +13,22 @@ divide <- function(col1, col2){
   return(r) 
 }
 
+# change the components of the matrix to numeric
 m$a = as.numeric(m$a)
 m$b = as.numeric(m$b)
 
-
+# split the matrix by each group
 group = split(m, name)
+print(group)
+
+#for each group in k, divide column a by column b
+
 for (k in group){
-  #print(divide(k$a, k$b))
-  print(k)
+  print(divide(k$a, k$b))
+  #print(k)
 }
-  
-  
-  
-  
-  
-  
+ 
+
   
   
   
